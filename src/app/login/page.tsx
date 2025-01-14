41332778f7fa6e7ce68a9ff09cf2dc4326e6ed10
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-15 00:34:09 +0530

    coonected database

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function LoginPage(){
     return(
         <div>
-            <h1>Login</h1>
+            <h1 className="text-center text-white text-3xl" >Login</h1>
         </div>
     )
 }
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection=mongoose.connection;
+
+        connection.on('connected',()=>{
+            console.log("Nongodb successfully connected");
+
+        })
+        connection.on('error',(err)=>{
+            console.log("Nongodb successfully not connected");
+
+        })
+        
+    } catch (error) {
+        
+    }
+}
