1d25e97cca16a31d4e99461232017d6165229f00
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-17 23:28:55 +0530

    added a use page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,7 +2,7 @@ export default function UserProfile({params}:any){
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>User Profile</h1>
-            <hr><p className="text-4xl">Profile Page{params.id}</p></hr>
+            <hr/><p className="text-4xl">Profile Page {params.id}</p>
         </div>
     )
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage(){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile Page</h1>
+            <hr/><p className="text-4xl"> Main Profile Page </p>
+        </div>
+    )
+}
