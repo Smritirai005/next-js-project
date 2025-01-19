07ea6aab692bcab3adf6457a7802f342856c509d
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-19 19:51:34 +0530

    made the signup page more dynamic

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
@@ -8,6 +8,7 @@ import axios from "axios";
 
 
 export default function SignupPage(){
+    const router=useRouter();
     const [user,setUser]=React.useState({
         email:"",
         password:"",
@@ -17,6 +18,15 @@ export default function SignupPage(){
     const onSignup=async ()=>{
         
     }
+    useEffect(()=>{
+        if(user.email.length>0 && user.password.length>0 && user.username.length>0){
+            setButtonDisabled(false);
+        }
+        else{
+            setButtonDisabled(true);
+        }
+    })
+    const [buttonDisabled,setButtonDisabled]=React.useState(false)
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h2>Signup</h2>
@@ -45,7 +55,7 @@ export default function SignupPage(){
             placeholder="password"
             />
             <button onClick={onSignup} className="px-6 py-2 rounded-lg bg-blue-500 text-white font-semibold shadow-md hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-300 transition duration-300">
-                Sign Up
+                {buttonDisabled ? "No signup" :"signup"}
             </button>
             <Link href="/login" className="text-white " >Visit Login page</Link>
 
