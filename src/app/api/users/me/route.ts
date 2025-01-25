9367c1ed00f580d67383c465df9b043451e9db3e
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-25 19:43:57 +0530

    taking data from the token

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,30 @@
+import { getDatafromToken } from "@/helpers/getDatafromToken";
+import { NextRequest,NextResponse } from "next/server";
+import User from "@/models/userModel";
+import {connect} from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request:NextRequest) {
+
+    try {
+        const userID=await getDatafromToken(request);
+        const user=await User.findOne({_id: userID}).
+        select("-password");
+        return NextResponse.json({
+            message: "User found",
+            data:user
+        })
+
+        
+    } catch (error:any) {
+        return NextResponse.json({error:error.message},
+            {status:400}
+        );
+        
+    }
+
+
+    
+}
+
