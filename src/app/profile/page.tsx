d83e41b0a5ba1b8fc2f50b404d182bbb3097a4e9
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-25 20:13:01 +0530

    feature to get all the user details

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
+import React,{ useState }from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 
 export default function ProfilePage(){
     const router=useRouter()
+    const [data,setData]=useState("nothing")
     const logout= async () =>{
         try {
             await axios.get('/api/users/logout')
@@ -18,14 +20,25 @@ export default function ProfilePage(){
             toast.error(error.message);
             
         }
+    }
+    const getUserDetails= async () =>{
+        const res=await axios.get('/api/users/me')
+        console.log(res.data)
+        setData(res.data.data._id)
+       
+    
 
     }
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile Page</h1>
+            <h2>{data ==="nothing"? " Nothing here":<Link href={`/profile/${data}`}></Link>}</h2>
             <hr/><p className="text-4xl"> Main Profile Page </p>
             <button onClick={logout} className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
                 Logout
+            </button>
+            <button onClick={getUserDetails} className="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-red-600">
+                Get User Details
             </button>
         </div>
     )
