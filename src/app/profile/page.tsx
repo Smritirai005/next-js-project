ac2a606cd2bca0247db0813f069e0618b4f7ab70
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-25 17:45:10 +0530

    added a logout button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,9 @@ export default function ProfilePage(){
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile Page</h1>
             <hr/><p className="text-4xl"> Main Profile Page </p>
+            <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
+                Logout
+            </button>
         </div>
     )
 }
