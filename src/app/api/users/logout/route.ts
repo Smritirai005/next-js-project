9dbde546e104b592c9f3ff17cec3b143a2164a6a
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-25 18:00:48 +0530

    added the logout functionality

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest,NextResponse } from "next/server";
 
+
 export async function GET(){
     try {
         const response=NextResponse.json({
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+
 export default function ProfilePage(){
+    const router=useRouter()
+    const logout= async () =>{
+        try {
+            await axios.get('/api/users/logout')
+            toast.success('Logout Successful')
+            router.push('/login')
+            
+        } catch (error:any) {
+            console.log(error.message);
+            toast.error(error.message);
+            
+        }
+
+    }
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile Page</h1>
             <hr/><p className="text-4xl"> Main Profile Page </p>
-            <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
+            <button onClick={logout} className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
                 Logout
             </button>
         </div>
