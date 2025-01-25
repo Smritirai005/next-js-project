5030b7d5e9a872cd41f314535076b094ebf09847
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-25 20:20:43 +0530

    fetched user detail

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -32,12 +32,12 @@ export default function ProfilePage(){
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile Page</h1>
-            <h2>{data ==="nothing"? " Nothing here":<Link href={`/profile/${data}`}></Link>}</h2>
+            <h2>{data ==="nothing"? " Nothing here":<Link href={`/profile/${data}`}>{data}</Link>}</h2>
             <hr/><p className="text-4xl"> Main Profile Page </p>
             <button onClick={logout} className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
                 Logout
             </button>
-            <button onClick={getUserDetails} className="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-red-600">
+            <button onClick={getUserDetails} className="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">
                 Get User Details
             </button>
         </div>
