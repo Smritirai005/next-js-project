61ebc89eb1d5ed4ae2b47c82c2d0ed3491f2831e
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-01-25 16:03:14 +0530

    made a async call to user profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,9 @@
-export default function UserProfile({params}:any){
-    return(
+export default async function UserProfile({ params }: { params: { id: string } }) {
+    return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>User Profile</h1>
-            <hr/><p className="text-4xl">Profile Page {params.id}</p>
+            <hr />
+            <p className="text-4xl">Profile Page {params.id}</p>
         </div>
-    )
+    );
 }
